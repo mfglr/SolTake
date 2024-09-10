79aac0a51472ca93b1114ba5819fd6be12b159e4
mfglr <mfgglr@outlook.com>
2024-09-10 13:11:05 +0300

    production

---
+++ src/MySocailApp.Api/Controllers/Greetings.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MySocailApp.Api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class Greetings : ControllerBase
+    {
+        [HttpGet]
+        public string SayHi() => "Hi";
+    }
+}
