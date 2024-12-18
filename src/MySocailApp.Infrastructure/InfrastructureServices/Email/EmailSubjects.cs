138c4892290e3d0690d595653ca6f4df2bb2c06f
mfglr <mfgglr@outlook.com>
2024-12-18 09:00:48 +0300

    removed EmaılSubjects file.

--- src/MySocailApp.Infrastructure/InfrastructureServices/Email/EmailSubjects.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MySocailApp.Infrastructure.InfrastructureServices.Email
-{
-    public static class EmailSubjects
-    {
-        public static readonly string EmailVerification = "Email Verification";
-        public static readonly string RestPasswordMail = "Reset Your Password";
-    }
-}
