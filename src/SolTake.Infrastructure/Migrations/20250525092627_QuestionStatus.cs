e4fc1a324e4a4a33d3446be5d2e4e7bf07daab7b
mfglr <mfgglr@outlook.com>
2025-05-25 12:27:35 +0300

    Create new migration.

---
+++ src/SolTake.Infrastructure/Migrations/20250525092627_QuestionStatus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolTake.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuestionStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDraft",
+                table: "Questions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishedAt",
+                table: "Questions",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDraft",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "PublishedAt",
+                table: "Questions");
+        }
+    }
+}
